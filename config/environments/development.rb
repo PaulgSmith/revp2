5e1dae78222fb89b11bf1f10e0d498068d90f76a
PaulgSmith <PaulSmith.Contactinfo@gmail.com>
2023-04-20 10:03:23 -0700

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
   
-  config.hosts << /.*\.compute\.amazonaws\.com\z/
+  config.hosts << "ec2-54-71-28-118.us-west-2.compute.amazonaws.com"
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
